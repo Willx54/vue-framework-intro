106df6322a3eba1928ca47d77af5ce559197eccc
Will Priddle <will.priddle@nelsoncroom.co.uk>
2017-10-04 08:54:14 +0100

    first push

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <app-user></app-user>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
