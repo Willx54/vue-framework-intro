77e73628dfbff060b9418d366d34129be2a6a2f3
Will <w_priddle@hotmail.com>
2017-10-05 09:22:47 +0100

    finished this exercise

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <app-user></app-user>
+  	<div>
+  		<router-link to="/">Home</router-link>
+  		<router-link to="/blog">Blog</router-link>
+  		<router-link to="/account">Account</router-link>
+  	</div>
+  	<router-view></router-view>
+    <!-- <app-user></app-user> -->
+    <p>This is a test</p>
   </div>
 </template>
 
